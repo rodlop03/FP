b76337451e9df5131308b31639650cd79e5a94c4
rodlop03 <rodlop03@ucm.es>
2016-11-13 12:59:26 +0100

    intento de arreglar el contador de dias

--- main.cpp
+++ main.cpp
@@ -108,6 +108,43 @@ int askDay() {
 
 long int elapsed(int day, int month, int year, int day1, int month1,
                  int year1) {
+                 
+
+long int elapsed(int day, int month, int year, int day1, int month1,int year1, int daysMonth;) 
+{
+  if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 ||
+      month == 10 || month == 12) {
+    daysMonth = 31;
+  } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+    daysMonth = 30;
+  } else {
+    if (leap) {
+      daysMonth = 29;
+    } else {
+      daysMonth = 28;
+    }
+  }
+  
+  
+  
+  int countLeaps(int year1, int year) {
+  int leaps = 0;
+  for (int i = year1; i < year; i++) {
+    if (i % 4 == 0 && (i % 100 != 0 || i % 400 == 0)) {
+      leaps++;
+    }
+  }
+  cout << "num de bisiestos " << leaps << "\n";
+
+  return leaps;
+}
+  
+  
+  
+  
+  
+  long int result = (year-year1) *365+ (month-month1)+ (day-day1) + leaps
+  
   long int result = ((year - year1) * 365 + (month - month1) * 30 + (day - day1));
   cout << "dias transcurridos " << result << "\n";
 
@@ -223,4 +260,4 @@ int printMenu() {
     cin >> menu;
   }
   return menu;
-}
+}1
